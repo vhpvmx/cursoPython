be7e900a86acf7d5025997615ab5232ad480a96a
Kalel3 <KalelGonzalezPerez@Outlook.com>
2020-11-16 14:42:30 -0600

    algunos errores en la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,22 @@
 import sys
 
-num1 = int(sys.argv[1])
+try:
+    num1 = int(sys.argv[1])
 except Exception as e:
     num1 = 0
 
-num2 = int(sys.argv[2])
+print("num1")
+try:
+    num2 = int(sys.argv[2])
 except Exception as e:
-    num2 = 0 
+    num2 = 0
+print("num2")
+
+try:
+    op3 = "+,-,*,/"
+except Exception as e:
+    op3 = ""
+print("op3")
+
+if op3 == "+"
+print("1 + 2 = 3")
